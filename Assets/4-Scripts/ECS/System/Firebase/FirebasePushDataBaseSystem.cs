11030b3e06ed5f73ea37ad29d299a2dabb1eb672
Илья Мещеряков <qpkiraqp@gmail.com>
2023-03-04 21:46:16 +0300

    подключение лидеров и очков

---
+++ Assets/4-Scripts/ECS/System/Firebase/FirebasePushDataBaseSystem.cs
@@ -0,0 +1,31 @@
+﻿using Firebase.Database;
+using Unity.Entities;
+using UnityEngine;
+using NotImplementedException = System.NotImplementedException;
+
+namespace Game.ECS.System
+{
+    [UpdateAfter(typeof(FirebaseRegistrationSystem))]
+    public class FirebasePushDataBaseSystem : UpdateSystem
+    {
+        private DatabaseReference reference;
+        
+        protected override void OnCreate()
+        {
+            // Get the root reference location of the database.
+            DatabaseReference reference = FirebaseDatabase.DefaultInstance.RootReference;
+        }
+
+        protected override void OnUpdate()
+        {
+            throw new NotImplementedException();
+        }
+        
+        private void writeNewUser(string userId, string name, string email) {
+            UserStateNetwork user = new UserStateNetwork(name, email);
+            string json = JsonUtility.ToJson(user);
+
+            reference.Child("users").Child(userId).SetRawJsonValueAsync(json);
+        }
+    }
+}
---
+++ Assets/4-Scripts/States/User/UserStateNetwork.cs
@@ -0,0 +1,16 @@
+﻿namespace Game
+{
+    public class UserStateNetwork
+    {
+        public string username;
+        public string email;
+
+        public UserStateNetwork() {
+        }
+
+        public UserStateNetwork(string username, string email) {
+            this.username = username;
+            this.email = email;
+        }
+    }
+}
